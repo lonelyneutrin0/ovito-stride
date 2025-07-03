adcf7a22a0450648359d4446d5d256027ecb73b7
Hrishikesh Belagali <belagal1@msu.edu>
2025-07-03 19:24:03 -0400

    Added stride.py

--- ovito-stride/__init__.py
+++ ovito-stride/__init__.py
@@ -3,3 +3,5 @@ __author__ = "Hrishikesh Belagali"
 __author_email__ = "belagal1@msu.edu"
 __url__ = "https://github.com/lonelyneutrin0/ovito-stride"
 __description__ = "An OVITO Python modifier for STRIDE, a secondary structure classification algorithm."
+
+from . import stride as stride
